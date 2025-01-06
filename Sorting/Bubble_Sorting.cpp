91517be02498bec4779d717f4d866e25c0262fc5
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-01-06 05:57:08 +0000

    Bubble_Sorting

---
+++ Sorting/Bubble_Sorting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+void SortedArray(int* arr,int size){
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
+void BubbleSort(int* arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    
+}
+
+int main() {
+    int arr[] = {5, 4, 1, 3, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    BubbleSort(arr, size);
+    SortedArray(arr, size);
+    
+    
+    return 0;
+}
