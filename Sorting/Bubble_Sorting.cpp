e336c4192826c33a155fe7fa29e741e4cde3977d
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-01-06 05:59:29 +0000

    comments

--- Sorting/Bubble_Sorting.cpp
+++ Sorting/Bubble_Sorting.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+//Printing of the sorted arrays
 void SortedArray(int* arr,int size){
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
@@ -9,6 +10,7 @@ void BubbleSort(int* arr, int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
+                //Swapping of adjacent elemnts
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
