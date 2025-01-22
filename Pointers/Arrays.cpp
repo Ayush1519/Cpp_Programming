dc35513e1d1586c584005ab02859ccb7805ab5f8
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-01-22 11:05:30 +0000

    arrays

---
+++ Pointers/Arrays.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[10]={0,1,2,9};
+    cout<<arr<<endl;
+    cout<<arr[0]<<endl;
+    cout<<&arr<<endl;
+    cout<<*(arr+3)<<endl;
+
+
+
+}
