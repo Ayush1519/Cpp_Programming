c70e055c4d8eee6935ddc8534265d7f02c264fd8
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-01-24 04:59:40 +0000

    character

---
+++ Pointers/charArrays.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[10]={23,67,89};
+    cout<<arr<<endl;//address of the first memory block
+    char ch[10] ="abcde";
+    cout<< "Character Array:"<<ch<<endl;//all content of ch array
+
+    int *ptr =  &arr[0];
+    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
+
+    char *c= &ch[0];
+    cout<<c<<endl;
+    cout<<*(c+1)<<endl;
+    
+
+}
